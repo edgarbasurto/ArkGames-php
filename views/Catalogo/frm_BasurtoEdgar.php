d01e005c34919b463f3ca75aac3a3c94462b0cfd
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-02-23 22:52:11 -0500

    agregando boostrap

--- views/Catalogo/frm_BasurtoEdgar.php
+++ views/Catalogo/frm_BasurtoEdgar.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../../assets/css/master.css" />
     <link rel="stylesheet" href="../../assets/css/BasurtoEdgar.css" />
     <link rel="icon" href="../../assets/img/logo.svg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Catálogo - ArkGames</title>
 </head>
@@ -98,6 +99,7 @@
     <!----------------------------------------------------------------------------------------------->
 
     <script type="text/javascript" src="../../assets/js/Validacion_frmCompra.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
