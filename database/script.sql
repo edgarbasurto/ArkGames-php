9d42290835cfec10a8db55ce1966e24d8695ab07
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-24 19:50:51 -0500

    ok

--- database/script.sql
+++ database/script.sql
@@ -1,7 +1,12 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
 CREATE DATABASE
 /*!32312 IF NOT EXISTS*/
-ArkgamesBD
 /*2022-02-26 @Rafael1108 Corrijo nombre de BD por manejo de estandar del proyecto*/
+ArkgamesBD
+/*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE ArkgamesBD;
 /*TABLAS SUSCRIPCIÓN NOTICIAS --- HELEN*/
 DROP TABLE IF EXISTS suscripcion;
