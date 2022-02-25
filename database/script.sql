f99a5b38e69a3ab4883ba525dfe2483b3d5ac00c
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-02-24 19:45:31 -0500

    Correción en nombre de BD para manejo de estandar

--- database/script.sql
+++ database/script.sql
@@ -1,12 +1,8 @@
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET SQL_NOTES=0 */;
 CREATE DATABASE
 /*!32312 IF NOT EXISTS*/
-arkgamesbd
-/*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE arkgamesbd;
+ArkgamesBD
+/*2022-02-26 @Rafael1108 Corrijo nombre de BD por manejo de estandar del proyecto*/
+USE ArkgamesBD;
 /*TABLAS SUSCRIPCIÓN NOTICIAS --- HELEN*/
 DROP TABLE IF EXISTS suscripcion;
 CREATE TABLE `suscripcion` (
