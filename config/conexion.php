f913b4284d56f504d48e34c01eeb395e344f431e
hlbv17 <helenlbv17@gmail.com>
2022-02-26 13:39:11 -0500

    conexion como clase

--- config/conexion.php
+++ config/conexion.php
@@ -1,45 +1,31 @@
-<?php //
-//class Conectar
-//{
-//    protected $dbh;
-//
-//    protected function Conexion()
-//    {
-//        try {
-//
-//            $host = "localhost";
-//            $port = 3306;
-//            $user = "root"; //USUARIO DE ACCESO A LA BASE DE DATOS
-//            $password = ""; //CONTRASENIA DE ACCESO A LA BASE DE DATOS
-//            $dbname = "ArkgamesBD"; //NOMBRE DEL ESQUEMA DE LA BASE DE DATOS
-//
-//            $cadena = "mysql:host={$host};port={$port};dbname={$dbname}";
-//            $conectar = $this->dbh = new PDO($cadena,  $user, $password);
-//            return $conectar;
-//        } catch (Exception $e) {
-//            print "¡Error BD!: " . $e->getMessage() . "<br/>";
-//            print_r("ERROR DE CONEXION");
-//            die();
-//        }
-//    }
-//
-//    public function set_names()
-//    {
-//        return $this->dbh->query("SET NAMES 'utf8'");
-//    }
-//}
+<?php 
+class Conectar
+{
+    protected $dbh;
 
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=arkgamesbd', 'root', '');
-    // See the "errors" folder for details...
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    protected function Conexion()
+    {
+        try {
 
-<<<<<<< HEAD
             $host = "localhost";
             $port = 3306;
-            $user = "root"; //USURIO DE ACCESO A LA BASE DE DATOS
+            $user = "root"; //USUARIO DE ACCESO A LA BASE DE DATOS
             $password = ""; //CONTRASENIA DE ACCESO A LA BASE DE DATOS
-            $dbname = "arkgamesbd"; //NOMBRE DEL ESQUEMA DE LA BASE DE DATOS
-=======
->>>>>>> 0655a58ebf8aa5c37915b8b9572d00d95882e199
+            $dbname = "ArkgamesBD"; //NOMBRE DEL ESQUEMA DE LA BASE DE DATOS
 
+            $cadena = "mysql:host={$host};port={$port};dbname={$dbname}";
+            $conectar = $this->dbh = new PDO($cadena,  $user, $password);
+            return $conectar;
+        } catch (Exception $e) {
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            print_r("ERROR DE CONEXION");
+            die();
+        }
+    }
+
+    public function set_names()
+    {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+}
 ?>
