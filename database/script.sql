14d193f0c1d38be681d7adc16992c5635ca0b906
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-02-26 22:31:09 -0500

    Agregando tabla de categorias y productos

--- database/script.sql
+++ database/script.sql
@@ -19,3 +19,46 @@ CREATE TABLE `suscripcion` (
   `discord` varchar(2) NOT NULL,
   PRIMARY KEY (`id_suscripcion`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
+
+
+-- TABLA DE CATEGORIAS DE JUEGOS 
+-- EDGAR BASURTO
+
+DROP TABLE IF EXISTS `categorias`;
+
+CREATE TABLE `categorias` (
+  `id_categoria` int NOT NULL,
+  `nombre_categoria` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+
+LOCK TABLES `categorias` WRITE;
+
+INSERT INTO `categorias` VALUES (1,'Acción'),(2,'Arcade/aventura'),(3,'Deportivo'),(4,'Estrategia'),(5,'Simulación'),(6,'Juegos de mesa/cartas'),(7,'Juegos musicales');
+
+UNLOCK TABLES;
+
+
+-- TABLA DE PRODUCTOS
+-- EDGAR BASURTO
+
+DROP TABLE IF EXISTS `productos`;
+
+CREATE TABLE `productos` (
+  `id_producto` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `precio` decimal(6,2) NOT NULL,
+  `url_imagen` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `id_categoria` int NOT NULL,
+  `prod_estado` int NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `id_categoria_idx` (`id_categoria`),
+  CONSTRAINT `id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+LOCK TABLES `productos` WRITE;
+
+INSERT INTO `productos` VALUES (1,'Hitman 3',30.00,'../../assets/img/posters/Hitman3_poster.jpg',1,1),(2,'Back 4 Blood',35.00,'../../assets/img/posters/Back4Blood_poster.jpg',3,1),(3,'Deathloop',25.00,'../../assets/img/posters/Deathloop_poster.jpg',2,1),(4,'FIFA 22',60.75,'../../assets/img/posters/FIFA_22_Poster.jpg',5,1),(5,'Fornite',23.99,'../../assets/img/posters/fornite_poster.jpg',2,1),(6,'Forza Horison',45.60,'../../assets/img/posters/Forza-Horizon-poster.jpg',7,1),(7,'Guadians Galaxy',34.65,'../../assets/img/posters/GuardiansoftheGalaxy_poster.jpg',6,1),(8,'Halo Infinite',54.23,'../../assets/img/posters/halo-infinite_poster.jpg',4,1),(9,'It Takes 2',76.56,'../../assets/img/posters/ItTakesTwo_poster.jpg',7,1),(10,'Kena Bridge',12.43,'../../assets/img/posters/KenaBridgeOfSpirits_poster.jpg',2,1),(11,'Little Night',14.65,'../../assets/img/posters/LittleNightmares2_poster.jpg',5,1),(12,'Psychonauts',27.43,'../../assets/img/posters/Psychonauts2_poster.jpg',4,1),(13,'Resident Evil',102.85,'../../assets/img/posters/ResidentEvilVillage_poster.jpg',1,1),(14,'Returnal',41.89,'../../assets/img/posters/returnal_poster.jpeg',1,1),(15,'The Medium',63.05,'../../assets/img/posters/TheMedium_poster.jpg',1,1);
+
+UNLOCK TABLES;
