45f7e4d36ce59639698ca9d8d1139375fad3b156
Rafael1108 <edwin.larreab@ug.edu.ec>
2022-03-01 00:24:52 -0500

    ok

--- views/Templates/navBarBoostrap.php
+++ views/Templates/navBarBoostrap.php
@@ -1,81 +1,3 @@
- <header>
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-         <div class="container-fluid">
-             <a class="navbar-brand" href="#">
-                 <img src="../../assets/img/logo-icon-text.svg" alt="ArkGames" height="50" class="d-inline-block align-text-top" />
-             </a>
-             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                 <span class="navbar-toggler-icon"></span>
-             </button>
-
-
-             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
-
-                     <li class="nav-item">
-                         <a class="nav-link active" aria-current="page" href="../../index.html">
-                             <svg class="bi d-inline-block align-text-top mx-auto mb-1" width="24" style="color:white" height="24">
-                                 <use xlink:href="#home" />
-                             </svg>
-                             Home</a>
-                     </li>
-
-                     <li class="nav-item">
-                         <a class="nav-link active text white" aria-current="page" href="../Catalogo/BasurtoEdgar.php">
-                             <svg class="bi d-inline-block align-text-top mx-auto mb-1" width="24" height="24">
-                                 <use xlink:href="#grid" />
-                             </svg>
-                             Tienda</a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link active" aria-current="page" href="../Noticias/BernalHelen.php">
-                             <svg class="bi d-inline-block align-text-top mx-auto mb-1" width="24" height="24">
-                                 <use xlink:href="#table" />
-                             </svg>
-                             Noticias</a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link active" aria-current="page" href="../Soporte/EspinozaIvan.php">
-                             <svg class="bi d-inline-block align-text-top mx-auto mb-1" width="24" height="24">
-                                 <use xlink:href="#speedometer2" />
-                             </svg>
-                             Soporte</a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link active" aria-current="page" href="../Contacto/GualeEvelyn.php">
-                             <svg class="bi d-inline-block align-text-top mx-auto mb-1" width="24" height="24">
-                                 <use xlink:href="#people-circle" />
-                             </svg>
-                             Contacto</a>
-                     </li>
-
-                     <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li> -->
-
-                 </ul>
-                 <!-- <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form> -->
-             </div>
-         </div>
-     </nav>
- </header>
-
-
- <!-- 
- @Rafael1108 Menu OK     
  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
      <symbol id="home" viewBox="0 0 16 16">
          <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
@@ -143,4 +65,4 @@
              </ul>
          </div>
      </div>
- </nav> -->
+ </nav>
