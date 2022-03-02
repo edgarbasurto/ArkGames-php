e1b4d6083cba21b9219b777d6716dc4563f25ba4
hlbv17 <helenlbv17@gmail.com>
2022-03-02 18:57:12 -0500

    update suscripcion noticias

--- views/Noticias/eliminar_noticias.php
+++ views/Noticias/eliminar_noticias.php
@@ -3,7 +3,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../assets/css/master2.css" />
+        <link rel="stylesheet" href="../../assets/css/BernalHelen.css" />
         <link rel="shortcut icon" href="../../assets/img/logo.svg">
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
@@ -163,3 +163,4 @@
         <!--<script type="text/javascript" src="../../assets/js/Validacion_Noticias.js"></script>-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     </body>
+</html>
--- views/Noticias/frm_BernalHelen.php
+++ views/Noticias/frm_BernalHelen.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Página de noticas ArkGames" />
     <meta name="keywords" content="formulario,noticias,suscripción" />
-    <link rel="stylesheet" href="../../assets/css/master2.css" />
+    <link rel="stylesheet" href="../../assets/css/BernalHelen.css" />
     <link rel="shortcut icon" href="../../assets/img/logo.svg">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
