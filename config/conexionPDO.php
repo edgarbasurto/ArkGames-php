4eecabcd03303b04326a0e4a7186d8ac77589049
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2022-03-03 23:17:49 -0500

    Contraseña borrada

--- config/conexionPDO.php
+++ config/conexionPDO.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=ArkgamesBD', 'root', 'DBA#2021ug');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=ArkgamesBD', 'root', ''); //agregar contraseña de la base de datos
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
