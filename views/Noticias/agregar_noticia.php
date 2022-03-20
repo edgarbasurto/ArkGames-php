12a728a519bb1f7b9c52b5491daed8970f91e969
hlbv17 <helenlbv17@gmail.com>
2022-03-19 23:20:12 -0500

    +

--- views/Noticias/agregar_noticia.php
+++ views/Noticias/agregar_noticia.php
@@ -80,7 +80,7 @@
                     </div>
                     <div class="card-footer row ">
                         <div class="text-end">
-                            <a href="index.php" class="btn btn-secondary"><i class="fa-solid fa-right-from-bracket"></i> Salir</a>
+                            <a href="?c=noticias" class="btn btn-secondary"><i class="fa-solid fa-right-from-bracket"></i> Salir</a>
                             <button class="btn btn-primary" id="btnListo" type="submit" value="guardar">Listo</button>
                         </div>
                     </div>
