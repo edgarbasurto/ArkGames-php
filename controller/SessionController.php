7be35580aa7f50a3154fe9ce0ffb4d2b85828d9e
hlbv17 <helenlbv17@gmail.com>
2022-03-19 21:33:57 -0500

    0

--- controller/SessionController.php
+++ controller/SessionController.php
@@ -29,8 +29,8 @@ class SessionController
 
     public function end()
     {
-        setcookie("mySession", '00000000-0000-0000-0000-000000000000', time() - 3600, "/");
-        setcookie("mySession", '00000000-0000-0000-0000-000000000000', time() + 3600, "/");
+        setcookie("mySession", '0', time() - 3600, "/");
+        setcookie("mySession", '0', time() + 3600, "/");
         session_unset();
         session_destroy();
         header('Location: index.php');
